663810df4f405eca8940205f8fbd9f0bb7b7cd80
alibaba0010 <zakariyahali100@gmail.com>
2023-10-11 04:31:05 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for airbnb clone"""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
 
 
 class FileStorage:
-    """This class manages storage of airbnb models in JSON format"""
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
